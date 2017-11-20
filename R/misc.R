0dff77c49fb3e5f1fdf122888b2d7d1b54b5b680
hafen <rhafen@gmail.com>
2017-11-20 10:37:15 -0800

    Update to make travis ci happy

--- R/misc.R
+++ R/misc.R
@@ -5,7 +5,7 @@ message_nice <- function(...) {
 
 gf_gtable_filter <- function (x, keep, trim = FALSE) {
   matches <- x$layout$name %in% keep
-  x$layout <- x$layout[matches, , drop = FALSE]
+  x$layout <- x$layout[matches, , drop = FALSE] # nolint
   x$grobs <- x$grobs[matches]
   if (trim)
     x <- gtable::gtable_trim(x)
