80aaa6e393649f5b953df32c6438de1b09e47b6a
hafen <rhafen@gmail.com>
2019-10-01 17:57:44 -0700

    Remove lintr test

--- tests/testthat/test-zzz-lintr.R
+++ tests/testthat/test-zzz-lintr.R
@@ -1,7 +1,7 @@
-# https://github.com/jimhester/lintr
-if (requireNamespace("lintr", quietly = TRUE)) {
-  context("lints")
-  test_that("package Style", {
-    lintr::expect_lint_free(cache = TRUE)
-  })
-}
+# # https://github.com/jimhester/lintr
+# if (requireNamespace("lintr", quietly = TRUE)) {
+#   context("lints")
+#   test_that("package Style", {
+#     lintr::expect_lint_free(cache = TRUE)
+#   })
+# }
